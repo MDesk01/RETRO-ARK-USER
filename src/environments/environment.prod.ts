da332a12e3aa79901f7408fd1a13bb973934341f
Mateus Augusto™ <39173530+MDesk01@users.noreply.github.com>
2023-07-26 16:21:04 -0300

    add.26-07-2023

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,12 @@
 /*AUTOR: MATEUS AUGUSTO S. AZEVEDO*/
 export const environment = {
   firebase: {
-  apiKey: "AIzaSyAAxq9i7hakb8yMfLHUrvuZ2ucbAmdivcc",
-  authDomain: "conecte-app-a17f4.firebaseapp.com",
-  projectId: "conecte-app-a17f4",
-  storageBucket: "conecte-app-a17f4.appspot.com",
-  messagingSenderId: "492089139416",
-  appId: "1:492089139416:web:870681c249627d877e77ad"
+  apiKey: "xxxxxxxxxxxxxxxxxxxxxxxxx",
+  authDomain: "xxxxxxxxxxxxxxxxxxxxxxxxx",
+  projectId: "xxxxxxxxxxxxxxxxxxxxxxxxx",
+  storageBucket: "xxxxxxxxxxxxxxxxxxxxxxxxx",
+  messagingSenderId: "xxxxxxxxxxxxxxxxxxxxxxxxx",
+  appId: "xxxxxxxxxxxxxxxxxxxxxxxxx"
   },
   production: true
 };
